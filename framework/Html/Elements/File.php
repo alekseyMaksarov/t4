9bb179afcae775437312651bc742aa6dd28ae052
aleksey <aleksey@example.com>
2015-08-31 15:15:31 +0300

    Html/Elements/File.php render fix

--- framework/Html/Elements/File.php
+++ framework/Html/Elements/File.php
@@ -23,7 +23,7 @@ class File
         $this->setAttribute('style', 'position: absolute; left: 0; top: 0; width: 100%; height: 100%; transform: scale(20); letter-spacing: 10em; -ms-transform: scale(20); opacity: 0; cursor: pointer;');
 
         $ret = '
-        <div style="position: relative; overflow: hidden; height: 4ex; line-height: 4ex; display: inline-block">
+        <div style="position: relative; overflow: hidden; height: 4ex; line-height: 3ex; display: inline-block">
             <button type="button" style="float: right; height: 3.7ex;">' . $this->buttonText . '</button><input type="text" readonly="">' . parent::render() . '
         </div>
         ';
